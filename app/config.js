63e674e5ca481be0018cfb06040e7d1dd59bdd33
vasyl-n <vasyl.nrk@gmail.com>
2018-03-10 16:42:19 -0800

    sign up, login

--- app/config.js
+++ app/config.js
@@ -39,6 +39,22 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 /************************************************************/
 // Add additional schema definitions below
 /************************************************************/
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary()
+      user.string('username');
+      user.string('password');
+      user.string('salt');
+      user.timestamps()
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+
+
 
 
 module.exports = db;
--- shortly.js
+++ shortly.js
@@ -2,6 +2,9 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
+var session = require('express-session')
+var bcrypt = require('bcrypt');
+
 
 
 var db = require('./app/config');
@@ -22,26 +25,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+app.use(session({
+  secret: 'keyboard cat',
+  cookie: {}
+}));
 
-app.get('/', 
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
+}
+
+app.get('/',
 function(req, res) {
+  res.render('login');
+});
+
+app.get('/create', restrict, function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
-function(req, res) {
-  res.render('index');
-});
-
-app.get('/links', 
-function(req, res) {
+app.get('/links', restrict, function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.status(200).send(links.models);
   });
 });
 
-app.post('/links', 
-function(req, res) {
+app.post('/links', restrict, function(req, res) {
   var uri = req.body.url;
 
   if (!util.isValidUrl(uri)) {
@@ -72,11 +85,73 @@ function(req, res) {
   });
 });
 
+
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
 
+// Authenticate HERE!!
+app.post('/login', function(req, res) {
+  console.log(req.body)
+  var username = req.body.username;
+  var password = req.body.password;
 
+  new User({username: username}).fetch().then(function(user) {
+    if(user) {
+      var salt = user.attributes.salt;
+      var hash = bcrypt.hashSync(password, salt);
+
+      if (user.attributes.password === hash) {
+        res.send(200, 'Logged in')
+      } else {
+        res.send(400, 'Incorrect credentials')
+      }
+    }
+    res.send(400, 'Incorrect credentials')
+    //console.log('Here is the ', user.attributes.salt);
+  })
+
+  if(username == 'demo' && password == 'demo'){
+    req.session.regenerate(function(){
+      req.session.user = username;
+      res.redirect('/create');
+    });
+  }
+  else {
+    // console.log('here')
+    // res.redirect('login');
+  }
+});
+
+// Server Signup HTML
+app.get('/signup',
+function(req, res) {
+  res.render('signup');
+});
+
+
+// Creates new user
+app.post('/signup', function(req, res) {
+  new User({username: req.body.username}).fetch().then(function(model){
+    if(model){
+      res.send(400, 'User already exists')
+    } else {
+      var salt = bcrypt.genSaltSync(10);
+      var hash = bcrypt.hashSync(req.body.password, salt);
+      console.log(hash)
+      new User({
+        username: req.body.username,
+        password: hash,
+        salt: salt
+      })
+      .save()
+      .then(function(model) {
+        res.send(201);
+      });
+    }
+  })
+
+});
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
